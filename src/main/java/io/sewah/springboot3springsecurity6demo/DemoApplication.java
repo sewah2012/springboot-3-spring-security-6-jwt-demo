c7a5e462d9d4d4ee589125a70cd8fd59c98d98f3
Emmanuel Sahr Sewah <sewah2012@gmail.com>
2023-01-03 22:29:56 +0100

    initialize project and setup docker compose for db

---
+++ src/main/java/io/sewah/springboot3springsecurity6demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package io.sewah.springboot3springsecurity6demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationAndAuthorizationDemoUsingTheLatestSpringBoot3AndSpringSecurity6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationAndAuthorizationDemoUsingTheLatestSpringBoot3AndSpringSecurity6Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/sewah/springboot3springsecurity6demo/DemoApplicationTest.java
@@ -0,0 +1,13 @@
+package io.sewah.springboot3springsecurity6demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationAndAuthorizationDemoUsingTheLatestSpringBoot3AndSpringSecurity6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
